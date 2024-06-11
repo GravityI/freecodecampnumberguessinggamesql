2116481856261cf70841051b41f31a1d03187c06
Ícaro Peixoto <icaropeixoto7@gmail.com>
2024-06-11 16:51:48 +0000

    feat: Added user identification

--- number_guess.sh
+++ number_guess.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 RANDOM_NUMBER=$((1 + $RANDOM % 100))
-echo $RANDOM_NUMBER
+
+USERNAME_MENU ()
+{
+  echo "Enter your username:"
+  read USERNAME
+  USERNAME_RESULT=$($PSQL "SELECT name FROM users WHERE name = '$USERNAME'")
+  if [[ -z $USERNAME_RESULT ]]
+  then
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
+  else
+    GAMES_PLAYED=$($PSQL "SELECT games_played FROM users WHERE name = '$USERNAME'")
+    BEST_GAME=$($PSQL "SELECT best_game FROM users WHERE name = '$USERNAME'")
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  fi
+}
+
+USERNAME_MENU
